2df1b22bff51f04cf1a30742ce60b0e74fa2ca1a
AashuPatel <aashutoshpatelprofessional@gmail.com>
2025-08-05 12:56:43 +0530

    HW Card Button to horizontal and added progress bar during submission

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -48,6 +48,7 @@ const DialogContent = React.forwardRef<
         <span className="sr-only">Close</span>
       </DialogPrimitive.Close>
     </DialogPrimitive.Content>
+    
   </DialogPortal>
 ))
 DialogContent.displayName = DialogPrimitive.Content.displayName
